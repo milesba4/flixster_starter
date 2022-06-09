0937d43132b737b0adec3e606d5f7ff7b7382a0b
milesba4 <milesbaffour@gmail.com>
2022-06-09 13:46:34 -0400

    Creating function to fetch movie data given an input in the submission form

---
+++ script.js
@@ -0,0 +1,27 @@
+// global constants
+const movieFormEl = document.querySelector("#movie-form")
+const userInputEl = document.querySelector("#search-input")
+const movieResultsEl = document.querySelector("#movies-grid")
+const MY_API_KEY = "7a335bdf5d0d4591e95ae546290d36b6"
+
+//Event listener for submission
+movieFormEl.addEventListener("submit",getMovies)
+console.log("submit=",submit)
+
+
+//Functions
+
+async function getMovies(evt){
+//prevent reload
+evt.preventDefault()
+console.log("evt=",evt)
+let search =userInputEl.value
+console.log("search=",search)
+let URL = 'https://api.themoviedb.org/3/search/movie?api_key='+ MY_API_KEY + '&query=' + search
+console.log("url=", URL)
+const response = await fetch(URL)
+const movieData = await response.json();
+console.log("movieData=",movieData)
+
+
+}
