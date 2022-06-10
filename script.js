5bf542a6043e1a15c1870df3f6d820cdc33edaf0
milesba4 <milesbaffour@gmail.com>
2022-06-10 13:52:53 -0400

    Adding function that displays now playing movies when user first laods page

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let pageNum = 1
 //Event listener for submission
 movieFormEl.addEventListener("submit",handleSubmitForm)
 loadButtonEl.addEventListener("click",loadMore)
-console.log("click=",)
+
 
 
 //Functions
@@ -56,6 +56,7 @@ movieData.results.forEach(function(element){
 
 function handleSubmitForm(evt){
     movieResultsEl.innerHTML = " "
+    pageNum=1
     getMovies(evt)
 
 
@@ -66,3 +67,51 @@ async function loadMore(evt){
     const dataMovie = await getSqResult()
     displayMovies(dataMovie)
 }
+
+ async function getNowPlayingResults(){
+    let URL = 'https://api.themoviedb.org/3/movie/now_playing?api_key='+ MY_API_KEY + '&page=' + pageNum
+    console.log("url=", URL)
+    const response = await fetch(URL)
+    const NowPlayingData = await response.json();
+    console.log("NowPLayingData=", NowPlayingData)
+    return NowPlayingData
+    
+}
+
+
+async function getNowPlaying(){
+    console.log("here")
+    const NowPlayingData = await getNowPlayingResults()
+    DisplayNowPlaying(NowPlayingData)
+    console.log("NowPlayingData=",NowPlayingData)
+    
+}
+
+
+window.onload = function(){
+    getNowPlaying()
+    }
+
+function DisplayNowPlaying(NowPlayingData){
+    NowPlayingData.results.forEach(function(element){
+        document.getElementById("load-button").hidden = false;
+        movieResultsEl.innerHTML += 
+         '<div class = "grid-item movie-card">'
+         + '<img class = "movie-poster" src= https://image.tmdb.org/t/p/w500/' + element.poster_path +' alt= '+'"'+element.original_title +'" ' + ' width = 300 height = 300/>' + 
+         '<br>' + '<p class = "movie-title">'+ element.original_title +'</p>' +
+         '<br>'+ '<div class="votes"> <img class ="star" src="https://cdn-icons-png.flaticon.com/512/3004/3004112.png" height = 20 width = 20 >' + '<p class = "movie-votes">'+element.vote_average+'</p>'
+         '</div> </div>'
+       });
+       console.log("DisplayNowPlayingInner=",movieResultsEl )
+       ;
+      
+
+
+}
+
+
+
+
+
+
+
